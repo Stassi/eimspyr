282f66277eeac5ab271ed619713e148a33e19abb
Andreas Stassivik <andreas@stassi.net>
2021-10-14 13:15:49 -0700

    QueryType includes Valheim

--- src/valheimServerStatus.ts
+++ src/valheimServerStatus.ts
@@ -1,14 +1,19 @@
-import type { QueryResult } from 'gamedig'
+import type { QueryOptions, QueryResult, Type as QueryType } from 'gamedig'
 import gamedig from 'gamedig'
+
+function serverStatus(
+  options: Omit<QueryOptions, 'type'> & { type: QueryType | 'valheim' }
+): Promise<QueryResult> {
+  return gamedig.query(<QueryOptions>options)
+}
 
 export default function valheimServerStatus(
   host: string,
   port: number = 10011
 ): Promise<QueryResult> {
-  return gamedig.query({
-    // @ts-ignore
-    type: 'valheim',
+  return serverStatus({
     givenPortOnly: true,
+    type: 'valheim',
     host,
     port,
   })
