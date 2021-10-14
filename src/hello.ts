97b51b4b8cc4264b79ad0f2ea9d806f6d7199d92
Andreas Stassivik <andreas@stassi.net>
2021-10-14 04:29:00 -0700

    superfluous test, function removal

--- src/hello.ts
+++
@@ -1,3 +0,0 @@
-export default function hello(target: string): string {
-  return `hello ${target}`
-}
--- src/helloWorld.test.ts
+++
@@ -1,7 +0,0 @@
-import hello from './hello'
-
-describe('hello(world)', () => {
-  it('should return "hello world"', () => {
-    expect(hello('world')).toBe('hello world')
-  })
-})
