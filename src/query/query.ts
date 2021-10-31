fcd9bad7711550b4d30ad48b16ff4eae777b1273
Andreas Stassivik <andreas@stassi.net>
2021-10-30 17:44:42 -0700

    query implementation

---
+++ src/query/query.ts
@@ -0,0 +1,36 @@
+import type { RemoteInfo } from './createUDPSocket'
+import type { Socket } from 'node:dgram'
+import { Buffer } from 'node:buffer'
+import { createUDPSocket } from './createUDPSocket'
+import { handleUDPSocketError } from './handleUDPSocketError'
+
+export type Query = RemoteInfo & {
+  message: Buffer
+  socket: Socket
+}
+
+export default function query({
+  address,
+  message,
+  port,
+}: {
+  address?: string
+  message: Buffer
+  port?: number
+}): Promise<Query> {
+  return new Promise(
+    (resolve: (x: any) => unknown, reject: (error: Error) => void) => {
+      const socket: Socket = createUDPSocket({ resolve, reject })
+
+      socket.send(
+        message,
+        port,
+        address,
+        handleUDPSocketError({
+          reject,
+          socket,
+        })
+      )
+    }
+  )
+}
