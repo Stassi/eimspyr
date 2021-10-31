c2ace45ebaa2cfc7bed8950d9ffff4c135b75152
Andreas Stassivik <andreas@stassi.net>
2021-10-30 17:44:11 -0700

    createUDPSocket implementation

---
+++ src/query/createUDPSocket.ts
@@ -0,0 +1,39 @@
+import type { Socket } from 'node:dgram'
+import { createSocket } from 'node:dgram'
+import { Buffer } from 'node:buffer'
+import { handleUDPSocketError } from './handleUDPSocketError'
+
+export type RemoteInfo = {
+  address: string
+  family: string
+  port: number
+  size: number
+}
+
+export function createUDPSocket({
+  resolve,
+  reject,
+}: {
+  resolve: (x: any) => unknown
+  reject: (error: Error) => void
+}): Socket {
+  const socket: Socket = createSocket('udp4')
+
+  socket
+    .on(
+      'error',
+      handleUDPSocketError({
+        reject,
+        socket,
+      })
+    )
+    .on('message', (message: Buffer, remoteInfo: RemoteInfo): void => {
+      socket.close()
+      resolve({
+        message,
+        ...remoteInfo,
+      })
+    })
+
+  return socket
+}
