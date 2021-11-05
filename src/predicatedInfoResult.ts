56a762727344d4854bbdc808d16de73459219ead
Andreas Stassivik <andreas@stassi.net>
2021-11-05 16:18:45 -0700

    predicated infoQuery result implemented

---
+++ src/predicatedInfoResult.ts
@@ -0,0 +1,50 @@
+import { Buffer } from 'node:buffer'
+import type { ParsedDataTypes } from './readSRCDSResponse'
+import type { InfoResultAndRemaining, ReaderIntent } from './infoResultTypes'
+
+export type PredicatedReaderIntent = {
+  predicateMask: 0x01 | 0x10 | 0x20 | 0x40 | 0x80
+} & ReaderIntent<
+  | 'appID'
+  | 'keywords'
+  | 'platformIDLong'
+  | 'port'
+  | 'spectatorName'
+  | 'spectatorPort'
+>
+
+export type PredicatedInfoResult = InfoResultAndRemaining<
+  Partial<{
+    appID: bigint
+    keywords: string
+    platformIDLong: bigint
+    port: number
+  }>
+>
+
+export default function predicatedInfoResult({
+  extraDataFlag,
+  intents,
+  remaining: initialRemaining,
+}: {
+  extraDataFlag: number
+  intents: PredicatedReaderIntent[]
+  remaining: Buffer
+}): PredicatedInfoResult {
+  return intents.reduce(
+    (
+      { remaining: prevRemaining, ...prevProps }: { remaining: Buffer },
+      { name, predicateMask, reader }: PredicatedReaderIntent
+    ) => {
+      if (!!(extraDataFlag & predicateMask)) {
+        const { remaining, value }: ParsedDataTypes = reader(prevRemaining)
+        return {
+          ...prevProps,
+          [name]: value,
+          remaining,
+        }
+      } else return { ...prevProps, remaining: prevRemaining }
+    },
+    { remaining: initialRemaining }
+  )
+}
