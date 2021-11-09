7ca1308ce4cfd97807cd3c720077d960be1fe24a
Andreas Stassivik <andreas@stassi.net>
2021-11-08 18:02:03 -0800

    superfluous files removal

--- src/infoResultTypes.ts
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'node:buffer'
-import { DecodedDataTypes } from './transcoder/decoder'
-
-export type InfoResultAndRemaining<T> = T & { remaining: Buffer }
-
-export type ReaderIntent<Name> = {
-  name: Name
-  reader: (buffer: Buffer) => DecodedDataTypes
-}
