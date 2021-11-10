dce9efb4172fc662619363bc83a5301f9ef1f67c
Andreas Stassivik <andreas@stassi.net>
2021-11-10 02:53:55 -0800

    InfoResult named export

--- src/decodeInfoQuery/index.ts
+++ src/decodeInfoQuery/index.ts
@@ -1,8 +1,9 @@
+import type { InfoResult } from './decodeInfoQuery'
 import type { InfoResultInitial } from './infoResults'
 import { initial as decodeInfoQueryInitial } from './infoResults'
 import { initial as decoderIntentsInitial } from './decoderIntents'
 import decodeInfoQuery from './decodeInfoQuery'
 
-export type { InfoResultInitial }
+export type { InfoResult, InfoResultInitial }
 export { decodeInfoQueryInitial, decoderIntentsInitial }
 export default decodeInfoQuery
