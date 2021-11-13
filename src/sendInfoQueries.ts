6a382dbe30a7f91c4c0daacc1e98dc6629852546
Andreas Stassivik <andreas@stassi.net>
2021-11-12 17:28:43 -0800

    sendInfoQueries implementation

---
+++ src/sendInfoQueries.ts
@@ -0,0 +1,47 @@
+import type { Encoded } from './transcoder'
+import type { InfoResultInitial } from './decodeInfoQuery'
+import type { Query } from './query'
+import type { RemoteDestination } from './query/createUDPSocket'
+import { Buffer } from 'node:buffer'
+import {
+  decodeInfoQueryInitial,
+  decoderIntentsInitial as intents,
+} from './decodeInfoQuery'
+import query from './query'
+import { writeCharacter, writeLong, writeString } from './transcoder'
+
+export type InfoResponseTall = Query & Partial<{ challenge: number }>
+
+const message: Buffer = Buffer.concat(
+  [writeLong(-1), writeCharacter('T'), writeString('Source Engine Query')].map(
+    ({ buffer }: Encoded) => buffer
+  )
+)
+
+export default async function sendInfoQueries(
+  destination: RemoteDestination
+): Promise<InfoResponseTall[]> {
+  const initialQuery: Query = await query({
+      message,
+      ...destination,
+    }),
+    { size, message: remaining }: Query = initialQuery,
+    isChallenge: boolean = size === 9
+
+  if (!isChallenge) {
+    return [initialQuery]
+  } else {
+    const { challenge }: InfoResultInitial = decodeInfoQueryInitial({
+      intents,
+      remaining,
+    })
+
+    return [
+      { ...initialQuery, challenge },
+      await query({
+        message: Buffer.concat([message, writeLong(challenge).buffer]),
+        ...destination,
+      }),
+    ]
+  }
+}
