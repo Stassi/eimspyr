402bb10899f5b74da70a12691786e79b46983e40
Andreas Stassivik <andreas@stassi.net>
2021-11-13 00:20:35 -0800

    extraneous rollup plugins removal

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,15 @@
-import commonjs from '@rollup/plugin-commonjs'
 import { defineConfig } from 'rollup'
-import json from '@rollup/plugin-json'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
 import ts from 'rollup-plugin-ts'
 
 const rollupConfig = defineConfig({
+  external: ['node:buffer', 'node:dgram'],
   input: 'src/index.ts',
   output: {
     file: 'lib/valping.mjs',
     format: 'es',
     sourcemap: true,
   },
-  plugins: [
-    commonjs(),
-    json(),
-    nodeResolve({ preferBuiltins: true }),
-    terser(),
-    ts(),
-  ],
+  plugins: [ts()],
 })
 
 export default rollupConfig
