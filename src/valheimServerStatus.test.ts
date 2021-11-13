f0da38c6a07fdbe883f5e10ed44a2370fc317395
Andreas Stassivik <andreas@stassi.net>
2021-11-13 00:33:18 -0800

    sampleHost test replacement

--- src/valheimServerStatus.test.ts
+++ src/valheimServerStatus.test.ts
@@ -1,11 +1,10 @@
-import pkg from '../package.json'
 import valheimServerStatus from './__mocks__/valheimServerStatus'
 import zeroPlayersResponse from './mockedResponses/zeroPlayersGameDig'
 
-const { sampleHost: host } = pkg
-
 describe('valheimServerStatus', () => {
   it('should return the server status', async () => {
-    expect(await valheimServerStatus(host)).toEqual(zeroPlayersResponse)
+    expect(await valheimServerStatus('95.156.194.254')).toEqual(
+      zeroPlayersResponse
+    )
   })
 })
