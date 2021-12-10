b88831cd5d1825b020a916390a3096580739fbd1
Andreas Stassivik <andreas@stassi.net>
2021-12-10 13:31:26 -0800

    (cjs | mjs) package hybridization

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,11 +5,18 @@ import ts from 'rollup-plugin-ts'
 const rollupConfig = defineConfig({
   external: ['node:buffer', 'node:dgram'],
   input: 'src/index.ts',
-  output: {
-    file: 'lib/eimspyr.mjs',
-    format: 'es',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: 'lib/eimspyr.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/eimspyr.mjs',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
   plugins: [nodeResolve(), ts()],
 })
 
